f749d0a121a6a15df1e7cbd6f71175428a7208a4
Jonas <Jonas.sf07@gmail.com>
2022-11-16 23:02:54 -0300

    Versão Principal

--- app/src/main/java/com/example/aprendendocores20/SetIntermediario.java
+++ app/src/main/java/com/example/aprendendocores20/SetIntermediario.java
@@ -3,8 +3,10 @@ package com.example.aprendendocores20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.concurrent.RejectedExecutionHandler;
 
@@ -14,23 +16,50 @@ public class SetIntermediario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_intermediario);
+        //Delay
+        Handler delay = new Handler();
 
+        //Botões
         Button btnCorreto = findViewById(R.id.btnInterCorreto);
+        Button btnErrado1 = findViewById(R.id.btnInterErrado1);
+        Button btnErrado2 = findViewById(R.id.btnInterErrado2);
+
+        //Funçõoes
         btnCorreto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                btnErrado1.setEnabled(false);
+                btnErrado2.setEnabled(false);
                 btnCorreto.setText("Laranja - Violeta - Verde");
                 btnCorreto.setBackgroundTintList(getResources().getColorStateList(R.color.verde));
+                delay.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(SetIntermediario.this, "Correto", Toast.LENGTH_SHORT).show();
+                    }
+                },3000);
 
             }
         });
-        Button btnErrado1 = findViewById(R.id.btnInterErrado1);
+
         btnErrado1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 btnCorreto.setEnabled(false);
+                btnErrado2.setEnabled(false);
                 btnErrado1.setText("Violeta - Verde - Laranja");
                 btnErrado1.setBackgroundTintList(getResources().getColorStateList(R.color.vermelho));
+            }
+        });
+
+        btnErrado2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnCorreto.setEnabled(false);
+                btnErrado1.setEnabled(false);
+                btnErrado2.setText("Verde - Laranja - Violeta");
+                btnErrado2.setBackgroundTintList(getResources().getColorStateList(R.color.vermelho));
+
             }
         });
     }
