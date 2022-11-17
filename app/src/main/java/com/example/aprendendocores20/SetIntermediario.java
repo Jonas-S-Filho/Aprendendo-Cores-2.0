871a9511efeab91de8320919ff914471defa87a0
Jonas <Jonas.sf07@gmail.com>
2022-11-17 09:44:26 -0300

    Versão Principal

--- app/src/main/java/com/example/aprendendocores20/SetIntermediario.java
+++ app/src/main/java/com/example/aprendendocores20/SetIntermediario.java
@@ -1,13 +1,16 @@
 package com.example.aprendendocores20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
+import android.opengl.Visibility;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class SetIntermediario extends AppCompatActivity {
@@ -26,6 +29,7 @@ public class SetIntermediario extends AppCompatActivity {
         Button btnErrado1 = findViewById(R.id.btnInterErrado1);
         Button btnErrado2 = findViewById(R.id.btnInterErrado2);
         ImageButton voltarMenuInter = findViewById(R.id.btnVoltarmenuInter);
+        Button tryAgain = findViewById(R.id.btnTryAgain);
 
         //Funções e Interações
         //Opções
@@ -37,7 +41,7 @@ public class SetIntermediario extends AppCompatActivity {
                 btnCorreto.setText("Laranja - Violeta - Verde");
                 btnCorreto.setBackgroundTintList(getResources().getColorStateList(R.color.verde));
 
-                //Transição para tela de parabéns
+                //test Transição para tela de parabéns (Ainda falta)
                 delay.postDelayed(new Runnable() {
                     @Override
                     public void run() {
@@ -55,6 +59,8 @@ public class SetIntermediario extends AppCompatActivity {
                 btnErrado2.setEnabled(false);
                 btnErrado1.setText("Violeta - Verde - Laranja");
                 btnErrado1.setBackgroundTintList(getResources().getColorStateList(R.color.vermelho));
+
+
             }
         });
 
@@ -69,6 +75,14 @@ public class SetIntermediario extends AppCompatActivity {
             }
         });
 
+        //Tentar Novamente
+        tryAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {tryAgain();
+
+            }
+        });
+
         //Botão Voltar
         voltarMenuInter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,8 +91,14 @@ public class SetIntermediario extends AppCompatActivity {
             }
         });
     }
+    //Transição de Tela
     public void voltarMenuInter(){
         android.content.Intent intent = new Intent(this, MenuPrincipal.class);
+        startActivity(intent);
+    }
+    //-----------------------------------------------------------------------------------------
+    public void tryAgain(){
+        android.content.Intent intent = new Intent(this, SetIntermediario.class);
         startActivity(intent);
     }
 }
